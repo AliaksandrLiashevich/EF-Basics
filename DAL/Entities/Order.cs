9a87de9594ef6d375b07afa1409c51de1a57bf44
Aliaksandr Liashevich <aliaksandr.liashevich@gmail.com>
2019-03-10 18:48:07 +0300

    Add entity 'Customer' and its configuration. Organize new migration.

--- DAL/Entities/Order.cs
+++ DAL/Entities/Order.cs
@@ -6,7 +6,11 @@ namespace DAL.Entities
     {
         public int Id { get; set; }
 
+        public int CustomerId { get; set; }
+
         public decimal TotalPrice { get; set; }
+
+        public Customer Customer { get; set; }
 
         public virtual ICollection<OrderItem> OrderItems { get; set; }
     }
--- DAL/Migrations/201903101520270_order_and_order_item.cs
+++ DAL/Migrations/201903101520270_order_and_order_item.cs
@@ -1,8 +1,7 @@
+using System.Data.Entity.Migrations;
+
 namespace DAL.Migrations
-{
-    using System;
-    using System.Data.Entity.Migrations;
-    
+{    
     public partial class order_and_order_item : DbMigration
     {
         public override void Up()
